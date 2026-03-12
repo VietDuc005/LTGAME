4cf323c2f5ab0a6e9a4a5cb6b9c5637b8f3e1937
VietDuc005 <ducsmile111@gmail.com>
2026-03-12 11:38:27 +0700

    Lab c7 Here we go

---
+++ Lab_C7/Assets/ButtonTesst.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTesst : MonoBehaviour
+{
+    public void onClickStart()
+    {
+        Debug.Log("game start");
+    }
+        public void onClickExit()
+        {
+            Debug.Log("game exit");
+    }
+    public void onClickOptions()
+    {
+        Debug.Log("game options");
+    }
+}
