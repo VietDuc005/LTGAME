07b971136e9beab42c4c850b1c0027869e511c10
Nguyen Viet Duc <ducsmile1111@gamil.com>
2026-03-24 12:17:44 +0700

    Lab c9

---
+++ Lab_C9/Assets/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static string NamePlayer = "DucSmile";
+    public static int Level = 1;
+}
